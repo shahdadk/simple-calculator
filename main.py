a36f1de5521018996b2ef2e2debd984e69f62152
Shahdad <shahdadkompanizare@gmail.com>
2024-07-15 11:47:16 -0400

    lets fucking go

--- main.py
+++ main.py
@@ -21,21 +21,49 @@ class Calculate:
             return "Sorry, you can't divide by zero."
         else:
             return self.input1 / self.input2
-        
-x=0
-
-while x==0:
     
-    while True:
-        input1 = input("Hi what would you like the first number to be?")
-        try:
-            input1=float(input1)
-            is_number = True
-        except ValueError:
-            is_number= False
+    
+# Input 1 While Loop
+while True:
+    input1 = input("Hi what would you like the first number to be?")
+    try:
+        input1=float(input1)
+        break
+    except ValueError:
+        print("Not a valid number, please try again!")
+        continue
 
+# Input 2 While Loop
+while True:
+    input2 = input("What would you like your second number to be?")
+    try:
+        input2=float(input2)
+        break
+    except ValueError:
+        print("Not a valid number, do you know what a number is?")
+        continue
 
-    while True:
-        input2 = input("What would you like your second number to be?")
+instance1 = Calculate(input1, input2)
 
+#main calculator loop
+while True:
+    input3= input("What operation would you like to dooooo? 1 - Addition, 2 - Subtraction, 3 - Multiplication, 4 - Division, 5 - Quit")
 
+    if input3 == "1":
+        print(str(instance1.addition(input1, input2)))
+        continue
+    elif input3=="2":
+        print(str(instance1.subtraction(input1, input2)))
+        continue
+    elif input3=="3":
+        print(str(instance1.multiplication(input1, input2)))
+        continue
+    elif input3=="4":
+        print(str(instance1.division(input1, input2)))
+        continue
+    elif input3=="5":
+        print("Ok thanks for using our calculator")
+        break
+    else:
+        print("Not a valid input, please try again.")
+        continue
