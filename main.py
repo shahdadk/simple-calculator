1e197776eaecd7e701904fcc64c58185687e8188
Shahdad <shahdadkompanizare@gmail.com>
2024-08-13 16:20:44 -0400

    Made small modifcations to program, like calculation methods, still need to fix the calculateExpression method.

--- main.py
+++ main.py
@@ -2,32 +2,53 @@
 #class calculator
 class Calculate:
 
-    def __init__(self, input1, input2):
-        self.input1 = input1
-        self.input2 = input2
+    def __init__(self):
+        pass
 
     def addition(self, input1, input2):
-        return self.input1 + self.input2
+        sum = input1 + input2
+        return sum
     
     def subtraction(self, input1, input2):
-        return self.input1 - self.input2
+        difference = input1 - input2
+        return difference
     
     def multiplication(self, input1, input2):
-        return self.input1 * self.input2
+        product = input1 * input2
+        return product
     
     def division(self, input1, input2):
+        if input2 == 0:
+            if input1 / input1 == 1:
+                return "infinity"
+            else:
+                return "-infinity"
+
+    def addEntry(self, entry, expression):
+        expression += entry
+        return expression
+
+    def clearEntry(self, expression):
+        expession = expression[:-1]
+        print(expression)
+        return expression
+    
+    def allClear(self, expression):
+        expression=""
+        return expression
+    
+    def calculateExpression(self, expression):
         
-        if self.input2 == 0:
-            return "Sorry, you can't divide by zero."
-        else:
-            return self.input1 / self.input2
+        return result
+    
+
     
     
 # Input 1 While Loop
 while True:
     input1 = input("Hi what would you like the first number to be?")
     try:
-        input1=float(input1)
+        input1 = float(input1)
         break
     except ValueError:
         print("Not a valid number, please try again!")
@@ -43,7 +64,7 @@ while True:
         print("Not a valid number, do you know what a number is?")
         continue
 
-instance1 = Calculate(input1, input2)
+instance1 = Calculate()
 
 #main calculator loop
 while True:
@@ -52,20 +73,20 @@ while True:
     if input3 == "1":
         print(str(instance1.addition(input1, input2)))
         continue
-    elif input3=="2":
+    elif input3 == "2":
         print(str(instance1.subtraction(input1, input2)))
         continue
-    elif input3=="3":
+    elif input3 == "3":
         print(str(instance1.multiplication(input1, input2)))
         continue
-    elif input3=="4":
+    elif input3 == "4":
         print(str(instance1.division(input1, input2)))
         continue
-    elif input3=="5":
+    elif input3 == "5":
         while True:
             input1 = input("Hi what would you like the first number to be?")
             try:
-                input1=float(input1)
+                input1 = float(input1)
                 break
             except ValueError:
                 print("Not a valid number, please try again!")
@@ -75,13 +96,13 @@ while True:
         while True:
             input2 = input("What would you like your second number to be?")
             try:
-                input2=float(input2)
+                input2 = float(input2)
                 break
             except ValueError:
                 print("Not a valid number, do you know what a number is?")
                 continue
         instance1 = Calculate(input1, input2)
-    elif input3=="6":
+    elif input3 == "6":
         print("Ok thanks for using our calculator")
         break
     else:
